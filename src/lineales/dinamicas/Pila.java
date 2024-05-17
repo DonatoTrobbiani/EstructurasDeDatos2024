ca4baea2c857fd636f05dbdadcb568fb1766e1d9
DonatoTrobbiani <donato.trobbiani@est.fi.uncoma.edu.ar>
2024-05-17 16:26:18 -0300

    Creación TDA Pila dinámica

---
+++ src/lineales/dinamicas/Pila.java
@@ -0,0 +1,85 @@
+package lineales.dinamicas;
+
+class Pila {
+    private Nodo tope;
+
+    public Pila() {
+        this.tope = null;
+    }
+
+    public boolean apilar(Object nuevoElem) {
+        
+        //Crea un nuevo nodo delante de la antigua cabecera
+        Nodo nuevo = new Nodo(nuevoElem, this.tope);
+        
+        //Actualiza el tope para que apunte al nodo nuevo
+        this.tope = nuevo;
+        
+        //Nunca hay error de pila llena, tonces devuelve true
+        return true;
+    }
+
+    public boolean desapilar() {
+        boolean exito = false;
+        
+        //Corrobora que la pila no esté vacía
+        if (this.tope != null) {
+            //Actualiza el tope para que apunte al nodo siguiente
+            this.tope = this.tope.getEnlace();
+            exito = true;
+        }
+        
+        return exito;
+    }
+
+    public Object obtenerTope() {
+        Object elem = null;
+        
+        //Corrobora que la pila no esté vacía
+        if (this.tope != null) {
+            //Obtiene el elemento del tope
+            elem = this.tope.getElem();
+        }
+        
+        return elem;
+    }
+
+    //Comprueba si la pila está vacía
+    public boolean esVacia() {
+        return this.tope == null;
+    }
+
+    // Vacia la pila.
+    // Por el gargabe collector, no es necesario eliminar todos los nodos.
+    public void vaciar() {
+        this.tope = null;
+    }
+
+    public Pila clone() {
+        Pila clon = new Pila();
+        Pila aux = new Pila();
+        Nodo temp = this.tope;
+
+        while (temp != null) {
+            aux.apilar(temp.getElem());
+            temp = temp.getEnlace();
+        }
+        while (!aux.esVacia()) {
+            clon.apilar(aux.obtenerTope());
+            aux.desapilar();
+        }
+        return clon;
+    }
+        
+    public String toString() {
+        String cadena = "";
+        if (this.tope != null) {
+        Nodo aux = this.tope;    
+        while (aux != null) {
+            cadena += aux.getElem().toString() + " ";
+            aux = aux.getEnlace();
+            }
+        }
+        return cadena;
+    }
+}
