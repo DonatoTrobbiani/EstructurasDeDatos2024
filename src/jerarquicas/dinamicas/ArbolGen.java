1bd6a43f5520eb6e9934183d5c4edd866252c85b
DonatoTrobbiani <donato.trobbiani@est.fi.uncoma.edu.ar>
2024-05-30 17:13:27 -0300

    Finalización ArbolGen y TestArbolGen

--- src/jerarquicas/dinamicas/ArbolGen.java
+++ src/jerarquicas/dinamicas/ArbolGen.java
@@ -7,4 +7,317 @@ public class ArbolGen {
     public ArbolGen() {
         this.raiz = null;
     }
+
+    public boolean insertar(Object elemNuevo, Object elemPadre) {
+        boolean exito = true;
+        if (this.raiz == null) {
+            //Si el arbol está vacío, pone elemNuevo en la raíz
+            this.raiz = new NodoGen(elemNuevo, null, null);
+        } else {
+            //Si el arbol no está vacío, busca el padre
+            NodoGen nodoPadre = obtenerNodo(this.raiz, elemPadre);
+            
+            if (nodoPadre != null) {
+                NodoGen nodoNuevo = new NodoGen(elemNuevo, null, null);
+                if (nodoPadre.getHijoIzquierdo() == null) {
+                    nodoPadre.setHijoIzquierdo(nodoNuevo);
+                } else {
+                    NodoGen hijo = nodoPadre.getHijoIzquierdo();
+                    while (hijo.getHermanoDerecho() != null) {
+                        hijo = hijo.getHermanoDerecho();
+                    }
+                    hijo.setHermanoDerecho(nodoNuevo);
+                }
+            } else {
+                exito = false;
+            }
+        }
+        return exito;
+    }
+
+    private NodoGen obtenerNodo(NodoGen nodo, Object buscado) {
+        NodoGen resultado = null;
+        if (nodo != null) {
+            if (nodo.getElem().equals(buscado)) {
+                resultado = nodo;
+            } else {
+                NodoGen hijo = nodo.getHijoIzquierdo();
+                while (hijo != null && resultado == null) {
+                    resultado = obtenerNodo(hijo, buscado);
+                    hijo = hijo.getHermanoDerecho();
+                }
+            }
+        }
+        return resultado;
+    }
+
+    public boolean insertarPorPosicion(Object elemNuevo, int posPadre) {
+        boolean exito = true;
+        if (posPadre == 1) {
+            if (this.raiz == null) {
+                this.raiz = new NodoGen(elemNuevo, null, null);
+            } else {
+                exito = false;
+            }
+        } else {
+            exito = insertarPorPosicionAux(this.raiz, elemNuevo, posPadre - 1);
+        }
+        return exito;
+    }
+
+    private boolean insertarPorPosicionAux(NodoGen nodo, Object elemNuevo, int posPadre) {
+        boolean exito = false;
+        if (nodo != null) {
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null && !exito) {
+                posPadre--;
+                if (posPadre == 0) {
+                    NodoGen nodoNuevo = new NodoGen(elemNuevo, null, null);
+                    nodoNuevo.setHermanoDerecho(hijo);
+                    nodo.setHijoIzquierdo(nodoNuevo);
+                    exito = true;
+                } else {
+                    exito = insertarPorPosicionAux(hijo, elemNuevo, posPadre);
+                }
+                hijo = hijo.getHermanoDerecho();
+            }
+        }
+        return exito;
+    }
+
+    public boolean pertenece(Object elem) {
+        return obtenerNodo(this.raiz, elem) != null;
+    }
+
+    public Lista ancestros(Object elem) {
+        Lista lista = new Lista();
+        ancestrosAux(this.raiz, elem, lista);
+        return lista;
+    }
+
+    private boolean ancestrosAux(NodoGen nodo, Object elem, Lista lista) {
+        boolean encontrado = false;
+        if (nodo != null) {
+            if (nodo.getElem().equals(elem)) {
+                encontrado = true;
+            } else {
+                NodoGen hijo = nodo.getHijoIzquierdo();
+                while (hijo != null && !encontrado) {
+                    encontrado = ancestrosAux(hijo, elem, lista);
+                    if (encontrado) {
+                        lista.insertar(nodo.getElem(), 1);
+                    }
+                    hijo = hijo.getHermanoDerecho();
+                }
+            }
+        }
+        return encontrado;
+    }
+
+    public boolean esVacio() {
+        return this.raiz == null;
+    }
+
+    public int altura() {
+        return alturaAux(this.raiz);
+    }
+
+    private int alturaAux(NodoGen nodo) {
+        int altura = -1;
+        if (nodo != null) {
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                int alturaHijo = alturaAux(hijo);
+                if (alturaHijo > altura) {
+                    altura = alturaHijo;
+                }
+                hijo = hijo.getHermanoDerecho();
+            }
+            altura++;
+        }
+        return altura;
+    }
+
+    public int nivel(Object elem) {
+        return nivelAux(this.raiz, elem, 0);
+    }
+
+    private int nivelAux(NodoGen nodo, Object elem, int nivel) {
+        int nivelElem = -1;
+        if (nodo != null) {
+            if (nodo.getElem().equals(elem)) {
+                nivelElem = nivel;
+            } else {
+                NodoGen hijo = nodo.getHijoIzquierdo();
+                while (hijo != null && nivelElem == -1) {
+                    nivelElem = nivelAux(hijo, elem, nivel + 1);
+                    hijo = hijo.getHermanoDerecho();
+                }
+            }
+        }
+        return nivelElem;
+    }
+
+    public Object padre(Object elem) {
+        Object padre = null;
+        if (this.raiz != null) {
+            if (!this.raiz.getElem().equals(elem)) {
+                padre = padreAux(this.raiz, elem);
+            }
+        }
+        return padre;
+    }
+
+    private Object padreAux(NodoGen nodo, Object elem) {
+        Object padre = null;
+        if (nodo != null) {
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null && padre == null) {
+                if (hijo.getElem().equals(elem)) {
+                    padre = nodo.getElem();
+                } else {
+                    padre = padreAux(hijo, elem);
+                }
+                hijo = hijo.getHermanoDerecho();
+            }
+        }
+        return padre;
+    }
+
+    public Lista listarPreorden() {
+        Lista lista = new Lista();
+        listarPreordenAux(this.raiz, lista);
+        return lista;
+    }
+
+    private void listarPreordenAux(NodoGen nodo, Lista lista) {
+        if (nodo != null) {
+            //Visitar el nodo
+            lista.insertar(nodo.getElem(), lista.longitud() + 1);
+
+            //Llamado recursivo a los hijos de nodo
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                listarPreordenAux(hijo, lista);
+                hijo = hijo.getHermanoDerecho();
+            }
+        }
+    }
+
+    public Lista listarInorden() {
+        Lista lista = new Lista();
+        listarInordenAux(this.raiz, lista);
+        return lista;
+    }
+
+    private void listarInordenAux(NodoGen nodo, Lista lista) {
+        if (nodo != null) {
+            //Llamado recursivo al hijo izquierdo de nodo
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            if (hijo != null) {
+                listarInordenAux(hijo, lista);
+            }
+
+            //Visitar el nodo
+            lista.insertar(nodo.getElem(), lista.longitud() + 1);
+
+            //Llamado recursivo a los otros hijos de nodo
+            if (nodo.getHijoIzquierdo() != null) {
+                hijo = nodo.getHijoIzquierdo().getHermanoDerecho();
+                while (hijo != null) {
+                    listarInordenAux(hijo, lista);
+                    hijo = hijo.getHermanoDerecho();
+                }
+            }
+        }
+    }
+
+    public Lista listarPosorden() {
+        Lista lista = new Lista();
+        listarPosordenAux(this.raiz, lista);
+        return lista;
+    }
+
+    private void listarPosordenAux(NodoGen nodo, Lista lista) {
+        if (nodo != null) {
+            //Llamado recursivo a los hijos de nodo
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                listarPosordenAux(hijo, lista);
+                hijo = hijo.getHermanoDerecho();
+            }
+
+            //Visitar el nodo
+            lista.insertar(nodo.getElem(), lista.longitud() + 1);
+        }
+    }
+
+    public Lista listarPorNiveles() {
+        Lista lista = new Lista();
+        Cola q = new Cola();
+        if (this.raiz != null) {
+            q.poner(this.raiz);
+            while (!q.esVacia()) {
+                NodoGen nodo = (NodoGen) q.obtenerFrente();
+                q.sacar();
+                lista.insertar(nodo.getElem(), lista.longitud() + 1);
+                NodoGen hijo = nodo.getHijoIzquierdo();
+                while (hijo != null) {
+                    q.poner(hijo);
+                    hijo = hijo.getHermanoDerecho();
+                }
+            }
+        }
+        return lista;
+    }
+
+    public ArbolGen clone() {
+        ArbolGen clon = new ArbolGen();
+        clon.raiz = cloneAux(this.raiz);
+        return clon;
+    }
+
+    private NodoGen cloneAux(NodoGen nodo) {
+        NodoGen clon = null;
+        if (nodo != null) {
+            clon = new NodoGen(nodo.getElem(), null, null);
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            NodoGen ultimoClon = clon;
+            while (hijo != null) {
+                ultimoClon.setHijoIzquierdo(cloneAux(hijo));
+                ultimoClon = ultimoClon.getHijoIzquierdo();
+                hijo = hijo.getHermanoDerecho();
+            }
+        }
+        return clon;
+    }
+
+    public void vaciar() {
+        this.raiz = null;
+    }
+
+    public String toString() {
+        return toStringAux(this.raiz);
+    }
+
+    private String toStringAux(NodoGen nodo) {
+        String cadena = "";
+        if (nodo != null) {
+            //Visita el nodo
+            cadena += nodo.getElem().toString() + " -> ";
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                cadena += hijo.getElem().toString() + ", ";
+                hijo = hijo.getHermanoDerecho();
+            }
+            
+            //Comienza recorrido de los hijos de nodo llamando recursivamente para que cada hijo agregue su subcadena a la general
+            hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                cadena += "\n" + toStringAux(hijo);
+                hijo = hijo.getHermanoDerecho();
+            }
+        }
+        return cadena;
+    }
 }
---
+++ src/tests/jerarquicas/TestArbolGen.java
@@ -0,0 +1,101 @@
+package tests.jerarquicas;
+import jerarquicas.dinamicas.ArbolGen;
+import lineales.dinamicas.Lista;
+
+public class TestArbolGen {
+    public static void main(String[] args) {
+        testingArbolGen();
+    }
+
+    public static void testingArbolGen() {
+        System.out.println("TESTING ARBOL GENERAL");
+        ArbolGen arbolGen = new ArbolGen();
+        System.out.println(arbolGen.toString());
+
+        System.out.println("PROBANDO INSERTAR/*");
+        System.out.println("Inserta raiz \"D\":, espera TRUE: " + arbolGen.insertar("D", "padre"));
+        System.out.println("Inserta \"Program Files\" como hijo de \"D\", espera TRUE: " + arbolGen.insertar("Program Files", "D"));
+        System.out.println("Inserta \"Windows\" como hijo de \"D\", espera TRUE: " + arbolGen.insertar("Windows", "D"));
+        System.out.println("Inserta \"System32\" como hijo de \"Windows\", espera TRUE: " + arbolGen.insertar("System32", "Windows"));
+        System.out.println("Inserta \"Dona\" como hijo de \"D\", espera TRUE: " + arbolGen.insertar("Dona", "D"));
+
+        System.out.println("Inserta \"Git\" como hijo de \"Program Files\", espera TRUE: " + arbolGen.insertar("Git", "Program Files"));
+        System.out.println("Inserta \"Java\" como hijo de \"Program Files\", espera TRUE: " + arbolGen.insertar("Java", "Program Files"));
+        System.out.println("Inserta \"Python\" como hijo de \"Program Files\", espera TRUE: " + arbolGen.insertar("Python", "Program Files"));
+
+        System.out.println("Inserta \"Estructuras\" como hijo de \"Java\", espera TRUE: " + arbolGen.insertar("Estructuras", "Java"));
+        System.out.println("Inserta \"Desarrollo\" como hijo de \"Java\", espera TRUE: " + arbolGen.insertar("Desarrollo", "Java"));
+
+        System.out.println("Inserta \"Carpeta\" como hijo de \"System32\", espera TRUE: " + arbolGen.insertar("Carpeta", "System32"));
+
+        System.out.println("Inserta \"app\" como hijo de \"Windows\", espera TRUE: " + arbolGen.insertar("app", "Windows"));
+        System.out.println("Inserta \"drivers\" como hijo de \"Windows\", espera TRUE: " + arbolGen.insertar("drivers", "Windows"));
+
+        System.out.println(arbolGen.toString());
+
+        System.out.println("Inserta \"Hola\" como hijo de \"NADA\", espera FALSE: " + arbolGen.insertar("Hola", "NADA"));
+        System.out.println(arbolGen.toString());
+        System.out.println("/*FINALIZA PRUEBA INSERTAR");
+
+        System.out.println("PROBANDO INSERTAR POR POSICION/*");
+        System.out.println("Inserta \"Documents\" como hijo de \"Dona(13)\", espera TRUE: " + arbolGen.insertarPorPosicion("Documents", 13));
+        System.out.println("Inserta \"ArbolGen\" como hijo de \"Estucturas(5)\", espera TRUE: " + arbolGen.insertarPorPosicion("ArbolGen", 5));
+
+        System.out.println(arbolGen.toString());
+
+        System.out.println("Inserta \"Hola\" como hijo de \"NADA(0)\", espera FALSE: " + arbolGen.insertarPorPosicion("Hola", 0));
+
+        System.out.println(arbolGen.toString());
+        System.out.println("/*FINALIZA PRUEBA INSERTAR POR POSICION");
+
+        System.out.println("Comprueba si el arbol esta vacio, espera FALSE: " + arbolGen.esVacio());
+
+        System.out.println("Retorna altura del arbol, espera 3: " + arbolGen.altura());
+
+        System.out.println("Comprueba si \"Java\" pertenece al arbol, espera TRUE: " + arbolGen.pertenece("Java"));
+        System.out.println("Comprueba si \"NADA\" pertenece al arbol, espera FALSE: " + arbolGen.pertenece("NADA"));
+
+        System.out.println("Retorna nivel de \"Program Files\", espera 1: " + arbolGen.nivel("Program Files"));
+        System.out.println("Retorna nivel de \"Desarrollo\", espera 3: " + arbolGen.nivel("Desarrollo"));
+        System.out.println("Retorna nivel de \"NADA\", espera -1: " + arbolGen.nivel("NADA"));
+
+        System.out.println("Retorna padre de \"Desarrollo\", espera \"Java\": " + arbolGen.padre("Desarrollo"));
+        System.out.println("Retorna padre de \"NADA\", espera NULL: " + arbolGen.padre("NADA"));
+
+        System.out.println("Clona el arbol");
+        ArbolGen clon = arbolGen.clone();
+        System.out.println("Arbol clonado: ");
+        System.out.println(clon.toString());
+
+        System.out.println("Vacía el arbol origianl");
+        arbolGen.vaciar();
+        System.out.println("Arbol original: ");
+        System.out.println(arbolGen.toString());
+        System.out.println("Retorna altura del arbol, espera -1: " + arbolGen.altura());
+        System.out.println("Comprueba si el arbol esta vacio, espera TRUE: " + arbolGen.esVacio());
+
+        System.out.println("Lista con los ancestros de \"Desarrollo\"");
+        Lista ancestros = clon.ancestros("Desarrollo");
+        System.out.println(ancestros.toString());
+
+        System.out.println("Lista con los ancestros de \"NADA\", espera lista vacia");
+        ancestros = clon.ancestros("NADA");
+        System.out.println(ancestros.toString());
+
+        System.out.println("Lista el arbol clonado en preorden");
+        Lista preorden = clon.listarPreorden();
+        System.out.println(preorden.toString());
+
+        System.out.println("Lista el arbol clonado en inorden");
+        Lista inorden = clon.listarInorden();
+        System.out.println(inorden.toString());
+
+        System.out.println("Lista el arbol clonado en posorden");
+        Lista posorden = clon.listarPosorden();
+        System.out.println(posorden.toString());
+
+        System.out.println("Lista el arbol clonado por niveles");
+        Lista porNiveles = clon.listarPorNiveles();
+        System.out.println(porNiveles.toString());
+    }
+}
